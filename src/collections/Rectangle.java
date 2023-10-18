e80d487ca99e3e221db7938cabced64ed42fc042
2022179 <146199008+2022179@users.noreply.github.com>
2023-10-18 13:44:02 +0100

    added the rectangle enhanced loop on the collections tab

---
+++ src/collections/Rectangle.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package collections;
+
+/**
+ *
+ * @author Primm
+ */
+public class Rectangle {
+    Rectangle []rectangles = {new Rectangle (2,3), new Rectangle (4,1)};
+}
